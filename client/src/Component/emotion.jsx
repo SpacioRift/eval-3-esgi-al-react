fbbf1a0583ac79bdc7a354516ac78c242e0bf65b
Willebois Arthur <arthur.willebois@outlook.fr>
2025-06-12 15:01:25 +0200

    Ajout des appels api (incomplets pour le moment)

--- client/src/Component/emotion.jsx
+++ client/src/Component/emotion.jsx
@@ -7,11 +7,37 @@ import { faHeart as faRegularHeart } from "@fortawesome/free-regular-svg-icons";
 
 export default function Emotion() {
     const [emotion, setEmotion] = useState(false);
-    console.log(emotion);
+    async function handleClick() {
+        try{
+            if(!emotion){
+                useEffect(() => {
+                    fetch('http://localhost:3000/user/', { // Modifier l'url en fonction de l'API
+                        method: "POST"
+                    })
+                    .then(result => result.json())
+                    .then(data => setUserList(data))
+                });
+            }
+            else{
+                useEffect(() => { // Modifier l'url en fonction de l'API
+                    fetch('http://localhost:3000/user/', {
+                        method: "DELETE"
+                    })
+                    .then(result => result.json())
+                    .then(data => setUserList(data))
+                });
+            }
+            setEmotion(!emotion);
+        }
+        catch (error) {
+            console.error("Error toggling emotion:", error);
+        }
+        
+    }
     return (
     <>
         <div className = "button-container">
-            <FontAwesomeIcon icon={emotion?faRegularHeart:faSolidHeart} onClick={() => setEmotion(!emotion)} />
+            <FontAwesomeIcon icon={emotion?faRegularHeart:faSolidHeart} onClick={() => handleClick()} />
         </div>
     </>
 )

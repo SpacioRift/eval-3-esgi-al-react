99f67495755b7ade60c494c3b9a703c06ea61762
Willebois Arthur <arthur.willebois@outlook.fr>
2025-06-12 14:17:14 +0200

    Ajout du composant emotion
    Gestion des réactions en lien avec les postes

---
+++ client/src/Component/emotion.jsx
@@ -0,0 +1,17 @@
+import "./emotion.css";
+//import { FontAwesomeIcon } from '@fontawesome/react-fontawesome';
+import { faHeart as faSolidHeart } from "@fortawesome/free-solid-svg-icons";
+import { faHeart as faRegularHeart } from "@fortawesome/free-regular-svg-icons";
+
+
+export default function Emotion() {
+    const [emotion, setEmotion] = useState(false);
+    return (
+    <>
+        <div className = "button-container">
+            <FontAwesomeIcon icon={emotion?faRegularHeart:faSolidHeart} onClick={() => setEmotion(!emotion)} />
+        </div>
+    </>
+    
+)
+}

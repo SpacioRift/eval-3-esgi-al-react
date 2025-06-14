750c2076f60443fa5d8a22d8e40e0f7364b3867d
Mamba_Spirit <axell.ngandu@gmail.com>
2025-06-14 14:31:00 +0200

    création et login du user

---
+++ client/src/Component/Login/Login.jsx
@@ -0,0 +1,79 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom"
+import "./Login.css";
+
+export default function Login(){
+
+    const [user, setUser] = useState({email:'', password:'', nickname:''});
+    const [warning, setWarning] = useState('');
+
+    const toThisPage = useNavigate();
+    
+    const handleClick = ()=>{
+        toThisPage('/Inscription');
+    }
+
+    const handleClickToPost = () => {
+        toThisPage('/Posts');
+    }
+
+    const handleChange = (event) => {
+    setUser({...user, [event.target.name]:event.target.value});
+    }
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        try{
+            const res = await fetch('http://localhost:3000/auth/login', {
+                method:'POST',
+                headers:{'Content-Type': 'application/json'},
+                body:JSON.stringify(user),
+            });
+            
+            const data = await res.json();
+
+            if(res.ok == true){
+                console.log("Connexion success");
+                handleClickToPost();
+            }
+            else{
+                console.log("Not connected");
+                setWarning("email ou mot de passe incorrecte")
+                return;
+            }
+            
+            setWarning(data.warning);
+        }
+
+        catch(error){
+            setWarning("Erreur d'inscription");
+        }
+
+    };
+
+    return (
+
+        <div>
+            <form action="">
+                <div>
+                    <label htmlFor="email"><strong>Email</strong></label>
+                    <input name="email" type="email" placeholder="email" onChange={handleChange}/>
+                </div>
+
+                <div>
+                    <label htmlFor="password"><strong>Mot de passe</strong></label>
+                    <input name="password" type="password" placeholder="mot de passe" onChange={handleChange}/>
+                </div>
+                <button onClick={handleSubmit}>Log in</button>
+                <p>{warning}</p>
+                <button onClick={handleClick}>Créer un compte</button>
+
+            </form>
+        </div>
+
+    )
+
+}
+
+

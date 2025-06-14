a0d678d0656d5aaf71b5da5602eee47b8faaed93
Willebois Arthur <arthur.willebois@outlook.fr>
2025-06-14 15:33:24 +0200

    kops

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,9 @@ import './App.css'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Emotion from '../src/Component/Emotion/emotion.jsx'
 import PostList from './Page/Post/PostList.jsx'
-<<<<<<< HEAD
 import EditPost from './Component/Post/EditPost/EditPost.jsx';
-=======
 import Login from '../src/Component/Login/Login';
 import Inscription from '../src/Component/Inscription/Inscription';
->>>>>>> aea442fa55e11e6b812ed051744663ae7d46a287
-
 function App() {
 
   // rajouter id en fonction du post sélectionné
@@ -19,13 +15,10 @@ function App() {
         <BrowserRouter>
           <Routes>
             <Route path="/post" element={<PostList />} />
-<<<<<<< HEAD
             <Route path="/edit" element={<EditPost />} />
-=======
             <Route path="/" element={<Login/>} />
             <Route path="/Login" element={<Login/>} />
             <Route path="/Inscription" element={<Inscription/>} />
->>>>>>> aea442fa55e11e6b812ed051744663ae7d46a287
           </Routes>
         </BrowserRouter> 
       </div>
